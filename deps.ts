2c3baa924cac8c9886bfde913b64e701ca78fb77
mindon <mindon@gmail.com>
2024-09-12 14:14:17 +0800

    Update deps.ts
    remove deno.land

--- deps.ts
+++ deps.ts
@@ -1,9 +1,6 @@
-export { Tar, Untar } from "https://deno.land/std@0.213.0/archive/mod.ts";
-export { ensureFile } from "https://deno.land/std@0.129.0/fs/ensure_file.ts";
-export { ensureDir } from "https://deno.land/std@0.129.0/fs/ensure_dir.ts";
-export { EventEmitter } from "https://deno.land/std@0.129.0/node/events.ts";
-export { crc32, Crc32Stream } from "https://deno.land/x/crc32@v0.2.0/mod.ts";
-export * as path from "https://deno.land/std@0.129.0/path/mod.ts";
-export { readAll, writeAll } from "https://deno.land/std@0.129.0/io/util.ts";
-export { copy } from "https://deno.land/std@0.129.0/streams/conversion.ts";
-export { Buffer } from "https://deno.land/std@0.129.0/io/buffer.ts";
+export { Tar, Untar } from "jsr:@std/archive";
+export { ensureFile, ensureDir } from "jsr:@std/fs";
+export { EventEmitter } from "node:events";
+export { crc32, Crc32Stream } from "https://raw.githubusercontent.com/deno-library/crc32/master/mod.ts";
+export * as path from "jsr:@std/path";
+export { copy, Buffer, readAll, writeAll } from "jsr:@std/io";
